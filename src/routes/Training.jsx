01a3e9f40ad0695294d3365b999fd137df77f86d
DimitarK13 <dimikalapocev@gmail.com>
2024-10-16 14:15:22 +0200

    feat(route: training): create and style

---
+++ src/routes/Training.jsx
@@ -0,0 +1,28 @@
+import Card from '../components/Card';
+
+import manageIcon from '../assets/manage-icon.svg';
+import presentationIcon from '../assets/presentation-icon.svg';
+
+import classes from '../css/Home.module.css';
+
+const Training = () => {
+  return (
+    <>
+      <div className={classes.cards}>
+        <Card
+          title="Organisation & Management Guidelines"
+          postsNumber="12"
+          icon={manageIcon}
+          href="/organisation-and-management-guidelines"
+        />
+        <Card
+          title="How-to Processes"
+          postsNumber="25"
+          icon={presentationIcon}
+          href="/how-to-processes"
+        />
+      </div>
+    </>
+  );
+};
+export default Training;
