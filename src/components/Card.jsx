31468728c070b0d576d5935123bde4eeccde6fa4
DimitarK13 <dimikalapocev@gmail.com>
2024-10-16 09:08:57 +0200

    feat(component: card): create and style

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+import classes from './Card.module.css';
+
+const Card = ({ title, postsNumber, icon, href }) => {
+  return (
+    <Link to={href} className={classes.card}>
+      <img className={classes.icon} src={icon} alt={title} />
+      <h2 className={classes.heading}>{title}</h2>
+      <p className={classes.info}>{postsNumber} Articles</p>
+    </Link>
+  );
+};
+export default Card;
