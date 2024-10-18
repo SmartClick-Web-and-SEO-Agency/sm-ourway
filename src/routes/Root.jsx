b9a66d9c74c23c24d429540a51bfefe403c206fe
DimitarK13 <dimikalapocev@gmail.com>
2024-10-18 09:06:24 +0200

    feat(global): dynamic header title

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,10 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 
 const Root = () => {
+  const [title, setTitle] = useState('SmartClick - OurWay');
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname !== '/') {
+      const arr = location.pathname.substring(1).split('-');
+      const capitalized = arr
+        .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
+        .join(' ');
+      setTitle(capitalized);
+    } else {
+      setTitle('SmartClick - OurWay');
+    }
+  }, [location]);
+
   return (
     <>
-      <Header title="SmartClick - OurWay" />
+      <Header title={title} />
 
       <main>
         <div className="container">
