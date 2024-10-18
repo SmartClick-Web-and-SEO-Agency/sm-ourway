d0e4498326ebe95b38bd47b96fadee074217a27e
DimitarK13 <dimikalapocev@gmail.com>
2024-10-18 09:07:55 +0200

    feat(component: article): format date & link to single post

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,10 +4,12 @@ import linkIcon from '../assets/link-icon.svg';
 import classes from './Article.module.css';
 
 const Article = ({ item }) => {
-  const date = new Date(item.modified);
+  const date = new Date(item.date);
   const formattedDate = `
-                ${date.getDate()}.${
-    date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+                ${
+                  date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+                }.${
+    date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
   }.${date.getFullYear()}
                 `;
 
@@ -15,7 +17,7 @@ const Article = ({ item }) => {
     <article className={classes.article}>
       <p className={classes.date}>{formattedDate}</p>
       <div className={classes.container}>
-        <Link to="TBD" className={classes.heading}>
+        <Link to={`/${item.slug}`} className={classes.heading}>
           {item.title?.rendered}
         </Link>
         <img className={classes.icon} src={linkIcon} alt="Link icon" />
