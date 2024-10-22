035e4722d7219d3432020b3d3640df60a5a56697
DimitarK13 <dimikalapocev@gmail.com>
2024-10-22 12:09:05 +0200

    feat(component: header): dynamic title and breadcrumbs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,62 @@
+import { Link, useLocation } from 'react-router-dom';
 import classes from './Header.module.css';
+import { useEffect, useState } from 'react';
 
-const Header = ({ title, location }) => {
+const Header = () => {
+  const location = useLocation();
+  const pathnames = location.pathname.split('/').filter((x) => x);
+
+  const [title, setTitle] = useState('SmartClick - OurWay');
+
+  useEffect(() => {
+    if (location.pathname !== '/') {
+      const pathSegments = location.pathname.split('/').filter((x) => x);
+      const lastSegment = pathSegments[pathSegments.length - 1];
+
+      const capitalized = lastSegment
+        .split('-')
+        .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
+        .join(' ');
+
+      setTitle(capitalized);
+    } else {
+      setTitle('SmartClick - OurWay');
+    }
+  }, [location]);
+
+  const formatBreadcrumb = (str) => {
+    return str
+      .split('-')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  };
+
   return (
     <header className={classes.header}>
-      <p className={classes.breadcrumbs}>
-        Home {location && '>'} {location}
-      </p>
-      <h1 className={classes.heading}>{title}</h1>
-      <input
-        className={classes.input}
-        type="text"
-        placeholder="Search OurWay..."
-      />
+      <div className="container">
+        <nav>
+          <p className={classes.breadcrumbs}>
+            <Link to="/">Home</Link>
+            {pathnames.map((value, index) => {
+              const to = `/${pathnames.slice(0, index + 1).join('/')}`;
+              return (
+                <span key={to}>
+                  {' > '}
+                  <Link to={to}>{formatBreadcrumb(value)}</Link>
+                </span>
+              );
+            })}
+          </p>
+        </nav>
+        <h1 className={classes.heading}>{title}</h1>
+        <input
+          className={classes.input}
+          type="text"
+          placeholder="Search OurWay..."
+        />
+      </div>
     </header>
   );
 };
+
 export default Header;
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,35 +1,19 @@
-import { useEffect, useState } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 
 import logoutIcon from '../assets/logout-icon.svg';
 
 const Root = () => {
   const navigate = useNavigate();
-  const [title, setTitle] = useState('SmartClick - OurWay');
-
-  const location = useLocation();
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     navigate('/login');
   };
 
-  useEffect(() => {
-    if (location.pathname !== '/') {
-      const arr = location.pathname.substring(1).split('-');
-      const capitalized = arr
-        .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
-        .join(' ');
-      setTitle(capitalized);
-    } else {
-      setTitle('SmartClick - OurWay');
-    }
-  }, [location]);
-
   return (
     <>
-      <Header title={title} />
+      <Header />
 
       <main>
         <div className="container">
